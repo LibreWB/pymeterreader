390b9f15b8a6c0d14389ae9d57a4a49a31c567f5
Norman Gehrsitz <gehrsitz.norman@student.dhbw-kahlsruhe.de>
2021-01-10 21:42:42 +0100

    Migrate ChannelInfo to @dataclass

--- pymeterreader/core/channel_info.py
+++ pymeterreader/core/channel_info.py
@@ -1,16 +1,19 @@
+from dataclasses import dataclass
+import typing as tp
+
+
+@dataclass()
 class ChannelInfo:
-    def __init__(self, uuid, interval, factor, last_upload, last_value):
-        """
-        Channel info structure
-        :param uuid: uuid of db entry to feed
-        :param interval: interval between readings in seconds
-        :param factor: multiply to original values, e.g. to conver kWh to Wh
-        :param last_upload: time of last upload to middleware
-        :param last_value: last value in middleware
-        """
-        # pylint: disable=too-many-arguments
-        self.uuid = uuid
-        self.interval = interval
-        self.factor = factor
-        self.last_upload = last_upload
-        self.last_value = last_value
+    """
+    Channel info structure
+    :param uuid: uuid of db entry to feed
+    :param interval: interval between readings in seconds
+    :param factor: multiply to original values, e.g. to conver kWh to Wh
+    :param last_upload: time of last upload to middleware
+    :param last_value: last value in middleware
+    """
+    uuid: str
+    interval: int
+    factor: float
+    last_upload: tp.Union[int, float]
+    last_value: tp.Union[int, float]
